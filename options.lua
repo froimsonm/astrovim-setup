08e73d6c3221e74b5ccbf709095b571e7cd0db40
Mikhail Froimson <m.froimson@sweatco.in>
2023-04-14 23:23:07 +0300

    Adjust scroll offset.

--- options.lua
+++ options.lua
@@ -7,6 +7,7 @@ return {
     spell = false, -- sets vim.opt.spell
     signcolumn = "auto", -- sets vim.opt.signcolumn to auto
     wrap = false, -- sets vim.opt.wrap
+    scrolloff = 12, -- Number of lines to keep above and below the cursor
   },
   g = {
     mapleader = " ", -- sets vim.g.mapleader
